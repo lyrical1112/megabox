e9b6dd716f3c4192abcce6989d2a2cb953f1d916
sonatine <lyrical1112@naver.com>
2022-03-20 22:24:06 +0900

    영화 차트 탭 메뉴

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,19 @@ var swiper = new Swiper('.swiper-container',{
         delay: 5000,
     },
 });
+
+//영화차트 탭 메뉴
+var movBtn = $(".movie_title > ul > li");
+var movCont = $(".movie_chart > div");
+
+movCont.hide().eq(0).show();
+
+movBtn.click(function(e){
+    e.preventDefault();
+    var target = $(this);
+    var index = target.index();
+    movBtn.removeClass("active");
+    target.addClass("active");
+    movCont.css("display","none");
+    movCont.eq(index).css("display","block");
+});
